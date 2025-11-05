afe61316ab489408df9033d8ed704b745db396ff
JESUTOLA OLUSEGUN <jesutolaolusegun@gmail.com>
2025-11-05 03:08:58 +0000

    change project id: 'project-3', title: 'BaseConnect' image to this image

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'base.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
